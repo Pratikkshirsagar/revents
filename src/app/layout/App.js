609b8540e1639255ac9afc2199dbfbe812a1e4c8
Pratik Kshirsagar <pratikkshirsagar@foliosolutions.in>
2020-03-25 21:22:46 +0530

    Navbar added

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h2>Re-Vents</h2>
-      </div>
+      <frames>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </frames>
     );
   }
 }
