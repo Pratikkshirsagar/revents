fe52fbb557c1167e70493a50594513fd2e2c4bbf
Pratik Kshirsagar <pratikkshirsagar@foliosolutions.in>
2020-03-26 13:59:23 +0530

    Filtering of code Done

--- src/features/event/EventDashboard/EventDashboard.js
+++ src/features/event/EventDashboard/EventDashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Button, List } from 'semantic-ui-react';
+import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
 import cuid from 'cuid';
