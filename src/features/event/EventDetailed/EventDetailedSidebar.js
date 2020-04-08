58efe477f7dbb4f6ae64ed805658e41323a85d0e
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-08 23:27:55 +0530

    events added

--- src/features/event/EventDetailed/EventDetailedSidebar.js
+++ src/features/event/EventDetailed/EventDetailedSidebar.js
@@ -1,23 +1,29 @@
 import React, { Fragment } from 'react';
 import { Segment, Item, Label } from 'semantic-ui-react';
 
-const EventDetailedSidebar = () => {
+const EventDetailedSidebar = ({ attendees }) => {
+  const isHost = false;
   return (
     <Fragment>
       <Segment textAlign="center" style={{ border: 'none' }} attached="top" secondary inverted color="teal">
-        2 People Going
+        {attendees && attendees.length} {attendees && attendees.length === 1 ? 'Person' : 'People'} Going
       </Segment>
       <Segment attached>
         <Item.Group divided>
-          <Item style={{ position: 'relative' }}>
-            <Label style={{ position: 'absolute' }} color="orange" ribbon="right">
-              Host
-            </Label>
-            <Item.Image size="tiny" src="/assets/user.png" />
-            <Item.Content verticalAlign="middle">
-              <Item.Header as="h3">Attendee Name</Item.Header>
-            </Item.Content>
-          </Item>
+          {attendees &&
+            attendees.map(attendee => (
+              <Item key={attendee.id} style={{ position: 'relative' }}>
+                {isHost && (
+                  <Label style={{ position: 'absolute' }} color="orange" ribbon="right">
+                    Host
+                  </Label>
+                )}
+                <Item.Image size="tiny" src={attendee.photoURL} />
+                <Item.Content verticalAlign="middle">
+                  <Item.Header as="h3">{attendee.name}</Item.Header>
+                </Item.Content>
+              </Item>
+            ))}
         </Item.Group>
       </Segment>
     </Fragment>
